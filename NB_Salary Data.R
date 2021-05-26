61b88995a9ae063d181ca60f3c05987d854fd717
Ranjeet Kashid <63395734+RanjeetKashid@users.noreply.github.com>
2021-05-26 09:27:41 +0530

    Update NB_Salary Data.R
    Deleted discretizing command, because no need to discretize since the data is already in factor format. And added accuracy for the Laplace model.

--- NB_Salary Data.R
+++ NB_Salary Data.R
@@ -7,32 +7,6 @@ str(salary_train)
 sum(is.na(salary_train))
 sum(is.na(salary_test))
 
-# Discretizing data - Training data
-
-salary_train_n <- names(which(sapply(salary_train[,-14], is.factor)))
-
-install.packages("CatEncoders")
-library(CatEncoders)
-
-for (i in salary_train_n){
-  encode <- LabelEncoder.fit(salary_train[,i])
-  salary_train[,i] <- transform(encode,salary_train[,i])
-}
-
-str(salary_train)
-
-# Discretizing data - Test data
-
-salary_test_n <- names(which(sapply(salary_test[,-14], is.factor)))
-
-for (i in salary_test_n){
-  encode <- LabelEncoder.fit(salary_test[,i])
-  salary_test[,i] <- transform(encode,salary_test[,i])
-}
-
-str(salary_test)
-
-
 # Defining i/p
 
 train_x <- salary_train[,1:13]
@@ -60,3 +34,5 @@ salary_classifier_l <- naiveBayes(salary_train$Salary ~.,data = train_x,laplace 
 salary_pred_l <- predict(salary_classifier_l,newdata = test_x)
 
 table2 <- table(salary_pred_l,test_y)
+accuracy2 <- (sum(diag(table2))/sum(table2))
+accuracy2 
